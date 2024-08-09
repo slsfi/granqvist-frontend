067a5752a7cf73f1b62c744edfdaa947d3850f16
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2024-08-09 22:42:47 +0300

    set up production configuration

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -2,15 +2,16 @@ type Config = { [key: string]: any }
 
 export const config: Config = {
   app: {
-    siteURLOrigin: "https://topelius.sls.fi",
-    projectNameDB: "topelius",
-    projectId: 10,
-    backendBaseURL: "https://testa-vonwright.sls.fi:8000/digitaledition",
+    siteURLOrigin: "https://granqvist.sls.fi",
+    projectNameDB: "granqvist",
+    projectId: 2,
+    backendBaseURL: "https://api.sls.fi/digitaledition",
     alternateFacsimileBaseURL: "",
     i18n: {
       languages: [
         { code: "sv", label: "Svenska", region: "FI" },
-        { code: "fi", label: "Suomi", region: "FI" }
+        { code: "en", label: "English", region: "GB" },
+        { code: "ar", label: "Arabic/English", region: "AR" }
       ],
       defaultLanguage: "sv",
       multilingualCollectionTableOfContents: false,
@@ -40,96 +41,121 @@ export const config: Config = {
     }
   },
   collections: {
-    addTEIClassNames: true,
-    replaceImageAssetsPaths: true,
-    enableLegacyIDs: true,
+    addTEIClassNames: false,
+    replaceImageAssetsPaths: false,
+    enableLegacyIDs: false,
     enableMathJax: false,
-    firstTextItem: {
-      216: "216_20280", 219: "219_19443", 220: "220_20122",
-      218: "218_20230_ch2", 210: "210_20548_ch1", 208: "208_18466_ch4",
-      207: "207_18464_ch1", 214: "214_20240_ch1", 203: "203_20217_ch1",
-      213: "213_18465_ch1", 202: "202_18467_ch1", 199: "199_18284",
-      221: "221_21422", 206: "206_20212_ch1", 201: "201_18471",
-      211: "211_20128", 200: "200_19870", 205: "205_20227_ch1",
-      215: "215_20568", 217: "217_20559_ch1", 204: "204_20322",
-      212: "212_20323", 209: "209_20479"
-    },
+    firstTextItem: {},
     frontMatterPages: {
-      cover: true,
+      cover: false,
       title: true,
-      foreword: true,
-      introduction: true
+      foreword: false,
+      introduction: false
     },
     highlightSearchMatches: true,
-    inlineIllustrations: [206],
-    mediaCollectionMappings: { 214: 44, 206: 19, 218: 19 },
+    inlineIllustrations: [],
+    mediaCollectionMappings: {},
     order: [
-      [216, 219, 220, 218, 210, 208, 207, 214, 203, 213,
-        202, 199, 221, 206, 201, 211, 200, 205, 215, 217,
-        204, 212, 209]
+      [537, 538, 539, 29]
     ]
   },
   ebooks: [
     {
-      title: "Bröd och bot",
-      filename: "norrback-brod-och-bot.epub",
+      title: "Birth and Childhood Among the Arabs",
+      filename: "granqvist-birth-and-childhood-among-the-arabs.pdf",
       externalFileURL: "",
-      coverURL: "",
+      coverURL: "assets/images/covers/books/granqvist_birth_and_childhood_among_the_arabs.jpg",
       downloadOptions: [
         {
-          url: "https://www.sls.fi/sv/utgivning/historiska-recept",
-          label: ""
+          url: "https://api.sls.fi/digitaledition/granqvist/files/147/pdf/147_11675_Birth_and_Childhood.pdf/granqvist-birth-and-childhood-among-the-arabs.pdf",
+          label: "PDF"
+        },
+        {
+          url: "https://api.sls.fi/digitaledition/granqvist/files/147/epub/granqvist-birth-and-childhood-among-the-arabs.epub/147.epub",
+          label: "EPUB"
         }
       ]
     },
     {
-      title: "Marriage Conditions in a Palestinian Village I (epub)",
-      filename: "marriage-conditions-1.epub",
-      externalFileURL: "https://api.sls.fi/digitaledition/granqvist/files/30/epub/30_11672_Marriage_Conditions_1.epub/",
-      coverURL: "",
+      title: "Child Problems Among the Arabs",
+      filename: "granqvist-child-problems-among-the-arabs.pdf",
+      externalFileURL: "",
+      coverURL: "assets/images/covers/books/granqvist_child_problems_among_the_arabs.jpg",
       downloadOptions: [
         {
-          url: "https://api.sls.fi/digitaledition/granqvist/files/30/epub/30_11672_Marriage_Conditions_1.epub/",
-          label: "EPUB"
+          url: "https://api.sls.fi/digitaledition/granqvist/files/148/pdf/148_11676_Child_Problems.pdf/granqvist-child-problems-among-the-arabs.pdf",
+          label: "PDF"
         },
         {
-          url: "https://api.sls.fi/digitaledition/granqvist/files/30/pdf/30_11672_Marriage_Conditions_1.pdf/",
-          label: "PDF"
+          url: "https://api.sls.fi/digitaledition/granqvist/files/148/epub/granqvist-child-problems-among-the-arabs.epub/148.epub",
+          label: "EPUB"
         }
       ]
     },
     {
-      title: "Marriage Conditions in a Palestinian Village I (pdf)",
-      filename: "marriage-conditions-1.pdf",
-      externalFileURL: "https://api.sls.fi/digitaledition/granqvist/files/30/pdf/30_11672_Marriage_Conditions_1.pdf/",
-      coverURL: "",
+      title: "Marriage Conditions in a Palestinian Village I",
+      filename: "granqvist-marriage-conditions-in-a-palestinian-village-1.pdf",
+      externalFileURL: "",
+      coverURL: "assets/images/covers/books/granqvist_marriage_conditions_in_a_palestinian_village_1.jpg",
       downloadOptions: [
         {
-          url: "https://api.sls.fi/digitaledition/granqvist/files/30/epub/30_11672_Marriage_Conditions_1.epub/",
-          label: "EPUB"
+          url: "https://api.sls.fi/digitaledition/granqvist/files/30/pdf/30_11672_Marriage_Conditions_1.pdf/granqvist-marriage-conditions-in-a-palestinian-village-1.pdf",
+          label: "PDF"
         },
         {
-          url: "https://api.sls.fi/digitaledition/granqvist/files/30/pdf/30_11672_Marriage_Conditions_1.pdf/",
+          url: "https://api.sls.fi/digitaledition/granqvist/files/30/epub/granqvist-marriage-conditions-in-a-palestinian-village-1.epub/30.epub",
+          label: "EPUB"
+        }
+      ]
+    },
+    {
+      title: "Marriage Conditions in a Palestinian Village II",
+      filename: "granqvist-marriage-conditions-in-a-palestinian-village-2.pdf",
+      externalFileURL: "",
+      coverURL: "assets/images/covers/books/granqvist_marriage_conditions_in_a_palestinian_village_2.jpg",
+      downloadOptions: [
+        {
+          url: "https://api.sls.fi/digitaledition/granqvist/files/31/pdf/31_11673_Marriage_Conditions_2.pdf/granqvist-marriage-conditions-in-a-palestinian-village-2.pdf",
           label: "PDF"
+        },
+        {
+          url: "https://api.sls.fi/digitaledition/granqvist/files/31/epub/granqvist-marriage-conditions-in-a-palestinian-village-2.epub/31.epub",
+          label: "EPUB"
+        }
+      ]
+    },
+    {
+      title: "Muslim Death and Burial",
+      filename: "granqvist-muslim-death-and-burial.pdf",
+      externalFileURL: "",
+      coverURL: "assets/images/covers/books/granqvist_muslim_death_and_burial.jpg",
+      downloadOptions: [
+        {
+          url: "https://api.sls.fi/digitaledition/granqvist/files/149/pdf/149_11677_Death_Burial.pdf/granqvist-muslim-death-and-burial.pdf",
+          label: "PDF"
+        },
+        {
+          url: "https://api.sls.fi/digitaledition/granqvist/files/149/epub/granqvist-muslim-death-and-burial.epub/149.epub",
+          label: "EPUB"
         }
       ]
     }
   ],
   page: {
     about: {
-      initialPageNode: "01-01"
+      initialPageNode: "01"
     },
     elasticSearch: {
       enableFilters: true,
-      enableSortOptions: true,
-      filterGroupsOpenByDefault: ["Years", "Type", "Genre", "Collection"],
+      enableSortOptions: false,
+      filterGroupsOpenByDefault: [],
       hitsPerPage: 15,
-      indices: ["topelius"],
+      indices: ["granqvist"],
       openReadingTextWithComments: false,
       textHighlightFragmentSize: 150,
       textHighlightType: "fvh",
       textTitleHighlightType: "fvh",
-      typeFilterGroupOptions: ["est", "com", "var", "inl", "tit", "fore"],
+      typeFilterGroupOptions: [],
       fixedFilters: [
         {
           terms: {
@@ -143,60 +169,7 @@ export const config: Config = {
         }
       ],
       additionalSourceFields: [],
-      aggregations: {
-        Years: {
-          date_histogram: {
-            field: "orig_date_sort",
-            calendar_interval: "year",
-            format: "yyyy"
-          }
-        },
-        Type: {
-          terms: {
-            field: "text_type",
-            size: 40,
-            order: {_key: "asc"}
-          }
-        },
-        Genre: {
-          terms: {
-            field: "publication_data.genre.keyword",
-            size: 40,
-            order: {_key: "asc"}
-          }
-        },
-        Collection: {
-          terms: {
-            field: "publication_data.collection_name.keyword",
-            size: 40,
-            order: {_key: "asc"}
-          }
-        },
-        LetterSenderName: {
-          terms: {
-            field: "sender_subject_name.keyword",
-            size: 100
-          }
-        },
-        LetterReceiverName: {
-          terms: {
-            field: "receiver_subject_name.keyword",
-            size: 100
-          }
-        },
-        LetterSenderLocation: {
-          terms: {
-            field: "sender_location_name.keyword",
-            size: 50
-          }
-        },
-        LetterReceiverLocation: {
-          terms: {
-            field: "receiver_location_name.keyword",
-            size: 50
-          }
-        }
-      }
+      aggregations: {}
     },
     foreword: {
       showURNButton: true,
@@ -205,16 +178,26 @@ export const config: Config = {
     home: {
       bannerImage: {
         altTexts: {
-          sv: "Porträtt av Zacharias Topelius",
-          fi: "Zacharias Topeliuksen muotokuva"
+          sv: "Svartvitt foto av fem personer som arbetar utomhus på en äng. Fyra mörkhyade personer i bakgrunden böjer sig ner med ryggarna mot kameran. En ljushyad kvinna i hatt i förgrunden ler in i kameran.",
+          en: "Black and white photo of five people working outdoors in a meadow. Four dark-skinned people in the background are bent over with their backs to the camera. A light-skinned woman in a hat in the foreground is smiling at the camera.",
+          ar: "Black and white photo of five people working outdoors in a meadow. Four dark-skinned people in the background are bent over with their backs to the camera. A light-skinned woman in a hat in the foreground is smiling at the camera."
         },
         intrinsicSize: {
-          height: null,
-          width: null
+          height: 469,
+          width: 1920
         },
-        orientationPortrait: true,
-        alternateSources: [],
-        URL: "assets/images/home-page-banner-portrait.jpg"
+        orientationPortrait: false,
+        alternateSources: [
+          {
+            srcset: "assets/images/granqvist_artas_surroundings_1930-1931_1920x469.avif 1920w",
+            type: "image/avif"
+          },
+          {
+            srcset: "assets/images/granqvist_artas_surroundings_1930-1931_1920x469.jpg 1920w",
+            type: "image/jpeg"
+          }
+        ],
+        URL: "assets/images/granqvist_artas_surroundings_1930-1931_1920x469.jpg"
       },
       portraitOrientationSettings: {
         imagePlacement: {
@@ -223,25 +206,25 @@ export const config: Config = {
         },
         siteTitleOnImageOnSmallScreens: false
       },
-      showContentGrid: false,
+      showContentGrid: true,
       showFooter: true,
       showSearchbar: false
     },
     index: {
       keywords: {
         maxFetchSize: 500,
-        showFilter: true,
+        showFilter: false,
         publishedStatus: 2
       },
       persons: {
         database: "elastic",
         maxFetchSize: 500,
-        showFilter: true,
+        showFilter: false,
         publishedStatus: 2
       },
       places: {
         maxFetchSize: 500,
-        showFilter: true,
+        showFilter: false,
         publishedStatus: 2
       },
       works: {
@@ -262,70 +245,71 @@ export const config: Config = {
       }
     },
     mediaCollection: {
-      showURNButton: true
+      showURNButton: false
     },
     text: {
-      defaultViews: ["readingtext", "comments", "facsimiles"],
-      defaultViewOptions: ["comments"],
-      showTextDownloadButton: true,
-      showURNButton: true,
-      showViewOptionsButton: true,
+      defaultViews: ["facsimiles"],
+      defaultViewOptions: [],
+      showTextDownloadButton: false,
+      showURNButton: false,
+      showViewOptionsButton: false,
       viewOptions: {
-        comments: true,
-        personInfo: true,
-        placeInfo: true,
-        emendations: true,
-        normalisations: true,
-        workInfo: true,
-        abbreviations: true,
-        paragraphNumbering: true,
-        pageBreakOriginal: true,
-        pageBreakEdition: true
+        comments: false,
+        personInfo: false,
+        placeInfo: false,
+        emendations: false,
+        normalisations: false,
+        workInfo: false,
+        abbreviations: false,
+        paragraphNumbering: false,
+        pageBreakOriginal: false,
+        pageBreakEdition: false
       },
       viewTypes: {
-        showAll: true,
-        readingtext: true,
-        comments: true,
+        showAll: false,
+        readingtext: false,
+        comments: false,
         facsimiles: true,
-        manuscripts: true,
-        variants: true,
-        illustrations: true,
-        legend: true,
+        manuscripts: false,
+        variants: false,
+        illustrations: false,
+        legend: false,
         metadata: false
       }
     },
     title: {
-      loadContentFromMarkdown: false,
-      showURNButton: true,
-      showViewOptionsButton: true
+      loadContentFromMarkdown: true,
+      showURNButton: false,
+      showViewOptionsButton: false
     }
   },
   component: {
     collectionSideMenu: {
-      sortableCollectionsAlphabetical: ["211", "215", "219", "220"],
-      sortableCollectionsChronological: ["215", "219", "220"],
+      sortableCollectionsAlphabetical: [],
+      sortableCollectionsChronological: [],
       sortableCollectionsCategorical: [],
       categoricalSortingPrimaryKey: "",
       categoricalSortingSecondaryKey: ""
     },
     contentGrid: {
-      includeEbooks: false,
-      includeMediaCollection: false,
-      mediaCollectionCoverURL: "",
+      includeEbooks: true,
+      includeMediaCollection: true,
+      mediaCollectionCoverURL: "assets/images/covers/photographs.jpg",
       mediaCollectionCoverAltTexts: {
-        sv: "Alt-text",
-        fi: "Alt-teksti"
+        sv: "Svartvitt foto av en ung kvinna som bär en stor korg fylld med föremål på sitt huvud. Hon står på en stenväg framför en karg kulle.",
+        en: "Black-and-white photo of a young woman carrying a large basket filled with items on her head. She stands on a stone road in front of a barren hill.",
+        ar: "Black-and-white photo of a young woman carrying a large basket filled with items on her head. She stands on a stone road in front of a barren hill."
       },
       showTitles: true
     },
     epub: {
-      showTOCButton: true,
-      showURNButton: true,
-      showViewOptionsButton: true
+      showTOCButton: false,
+      showURNButton: false,
+      showViewOptionsButton: false
     },
     facsimiles: {
-      imageQuality: 4,
-      showTitle: true
+      imageQuality: 1,
+      showTitle: false
     },
     mainSideMenu: {
       items: {
@@ -334,7 +318,7 @@ export const config: Config = {
         ebooks: true,
         collections: true,
         mediaCollections: true,
-        indexKeywords: false,
+        indexKeywords: true,
         indexPersons: true,
         indexPlaces: true,
         indexWorks: false
@@ -348,7 +332,7 @@ export const config: Config = {
     topMenu: {
       showAboutButton: true,
       showContentButton: true,
-      showElasticSearchButton: true,
+      showElasticSearchButton: false,
       showURNButton: false,
       showLanguageButton: true,
       showSiteLogo: true,
@@ -402,22 +386,22 @@ export const config: Config = {
       }
     },
     fullscreenImageViewer: {
-      imageQuality: 4
+      imageQuality: 1
     },
     referenceData: {
       URNResolverURL: "https://urn.fi/",
     },
     namedEntity: {
       showAliasAndPrevLastName: false,
-      showArticleData: false,
-      showCityRegionCountry: false,
+      showArticleData: true,
+      showCityRegionCountry: true,
       showDescriptionLabel: false,
-      showGalleryOccurrences: false,
-      showMediaData: false,
+      showGalleryOccurrences: true,
+      showMediaData: true,
       showOccupation: false,
       showOccurrences: true,
       showType: false,
-      useSimpleWorkMetadata: true
+      useSimpleWorkMetadata: false
     }
   }
 }
